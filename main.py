c93ed233461433804d80aafd25f25fcb770718c1
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2023-01-28 01:04:52 -0500

    Quiz initialization, dm messaging and reaction events 😵

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ bot = commands.Bot(command_prefix='?', intents = intents)
 # Variables
 quiz: Quiz = None
 
+# Events
+@bot.event
+async def on_reaction_add(reaction, user):
+    print(reaction.message)
+    await user.send("Hello")
+
 # Commands
 @bot.command()
 async def play(ctx):
     global quiz
 
-    if quiz and quiz.started:
+    if quiz and quiz.started():
         return await ctx.send("Sorry, but the match have already started 😅")
     
     message: str = ""
     if not quiz:
-        quiz = Quiz(ctx.message.author.name, 2)
+        quiz = Quiz(ctx.message.author.name)
         message = f'{ctx.message.author.mention} started a new Quizy game. Call your friends and tell them to join 🤩'
     else:
         added:bool = quiz.register_player(ctx.message.author.name)
@@ -31,11 +37,21 @@ async def play(ctx):
     await ctx.send(message)
 
 @bot.command()
-async def start(ctx):
+async def start(ctx, rounds:int=2):
     global quiz
 
-    quiz.started = True
+    inited = quiz.init_match(3)
+
+    if not inited:
+        if quiz.started():
+            await ctx.send("The match have already started 😅")
+        else:
+            await ctx.send("Sorry, but theres not enough players to start (at least 2) 😫")
+        return
+    
     await ctx.send("Let the play begin! 🤭")
+    first_question = quiz.pop_question()
+    await ctx.send(f"What would **{first_question[0]}** say:\n{first_question[1]}")
 
 @bot.command()
 async def end(ctx):
@@ -48,19 +64,15 @@ async def end(ctx):
 async def info(ctx):
     global quiz
 
-    if not quiz:
-        information = "Quiz not yet created 😬"
-    else:
-        information =f"""**Players:** {", ".join(quiz.players)}
-**Rounds:** {str(quiz.rounds)}
-"""
-        if quiz.started:
-            information += f"**Started**"
+    information = "Quiz not yet created 😬"
+    if quiz:
+        information = str(quiz)
 
     await ctx.send(information)
 
 @bot.command()
 async def joke(ctx):
+    await ctx.send("Let me think... 🤔")
     await ctx.send(tell_joke())
 
 load_dotenv()
--- quiz.py
+++ quiz.py
@@ -7,18 +7,45 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class Quiz:
 
-    def __init__(self, first_player: str, rounds: int):
-        self.players: set = {first_player}
+    def __init__(self, first_player: str):
+        self.players: dict = {first_player: 0}
         self.questions: list = []
-        self.rounds: int = rounds
-        self.started: bool = False
+        self.rounds: int = 0
+
+        self.__started: bool = False
     
     def __str__(self) -> str:
-        return "Este es el quiz"
+        information =f"**Players:** {', '.join(self.players)}"
+        if self.__started:
+            information += f"\n**Rounds:** {str(self.rounds)}"
+            information += f"\n**Started**"
+        
+        return information
 
-    def register_player(self, player) -> bool:
-        quantity: int = len(self.players)
-        self.players.add(player)
+    def register_player(self, player: str) -> bool:
+        if not self.__started:
+            quantity: int = len(self.players)
+            self.players[player] = 0
         return quantity != len(self.players)
 
+    def init_match(self, rounds: int) -> bool:
+        inited: bool = False
 
+        if (not self.__started) and (len(self.players) > 1):
+            inited = True
+            self.rounds = 3
+            for r in range(self.rounds):
+                self.questions += [(x, "**What kind of music do you enjoy the most?**\n**A.** Rock\n**B.** Electronic\n**C.** Sad music\n**D.** Rap") for x in self.players]
+            self.__started = True
+        
+        return inited
+    
+    def pop_question(self) -> tuple:
+        if self.__started and (len(self.questions) > 0):
+            return self.questions.pop(0)
+    
+    def answer_question(self, player: str, answer: str) -> bool: #No implemented
+        return True
+    
+    def started(self):
+        return self.__started
