6a309e52b15a91bbd2c496aa5f71f87e3bc65e9c
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2023-01-30 15:48:20 -0500

    Quiz class finished and properly tested 🐶

--- quiz.py
+++ quiz.py
@@ -5,47 +5,100 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+class Question:
+    def __init__(self, player: str, question: str):
+        self.player: str = player
+        self.question: str = question
+        self.answers: dict[str,str] = {}
+
+    def answer_question(self, player: str, answer: str) -> bool:
+        added: bool = False
+        if not (player in self.answers.keys()):
+            self.answers[player] = answer
+            added = True
+        return added
+
+    @property
+    def answer(self) -> str:
+        return self.answers[self.player]
+
 class Quiz:
 
     def __init__(self, first_player: str):
-        self.players: dict = {first_player: 0}
-        self.questions: list = []
-        self.rounds: int = 0
-
+        self.__players: set[str] = {first_player}
+        self.__questions: list[Question] = []
+        self.__answered_questions: list[Question] = []
+        self.__rounds: int = 0
         self.__started: bool = False
     
     def __str__(self) -> str:
-        information =f"**Players:** {', '.join(self.players)}"
+        information =f"**Players:** {', '.join(self.__players)}"
         if self.__started:
-            information += f"\n**Rounds:** {str(self.rounds)}"
+            information += f"\n**Rounds:** {str(self.__rounds)}"
             information += f"\n**Started**"
         
         return information
 
+    #Methods
     def register_player(self, player: str) -> bool:
+        quantity: int = len(self.__players)
         if not self.__started:
-            quantity: int = len(self.players)
-            self.players[player] = 0
-        return quantity != len(self.players)
+            self.__players.add(player)
+        return quantity != len(self.__players)
 
     def init_match(self, rounds: int) -> bool:
         inited: bool = False
 
-        if (not self.__started) and (len(self.players) > 1):
+        if (not self.__started) and (len(self.__players) > 1) and (rounds >= 1):
             inited = True
-            self.rounds = 3
-            for r in range(self.rounds):
-                self.questions += [(x, "**What kind of music do you enjoy the most?**\n**A.** Rock\n**B.** Electronic\n**C.** Sad music\n**D.** Rap") for x in self.players]
+            self.__rounds = rounds
+            for r in range(self.__rounds):
+                self.__questions += [Question(x, "**What kind of music do you enjoy the most?**\n**A.** Rock\n**B.** Electronic\n**C.** Sad music\n**D.** Rap") for x in self.__players]
             self.__started = True
         
         return inited
     
-    def pop_question(self) -> tuple:
-        if self.__started and (len(self.questions) > 0):
-            return self.questions.pop(0)
+    def answer_question(self, player: str, answer: str) -> bool:
+        answered: bool = False
+        if self.__started and (len(self.__questions) >= 1):
+            if player in self.players:
+                answered = self.__questions[0].answer_question(player, answer)
+                if (len(self.actual_question().answers) == len(self.players)):
+                    self.__answered_questions.append(self.__questions.pop(0))
+        return answered
+
+    def actual_question(self) -> Question:
+        actual: Question = None
+        if self.__started and (len(self.__questions) >= 1):
+            actual = self.__questions[0]
+        return actual
     
-    def answer_question(self, player: str, answer: str) -> bool: #No implemented
-        return True
+    def scores(self) -> dict[str, int]:
+        scores: dict[str, int] = {i:0 for i in self.players}
+        for question in self.__answered_questions:
+            for player in self.players:
+                if player != question.player:
+                    if question.answers[player] == question.answer:
+                        scores[player] += 1
+        return scores
     
-    def started(self):
+    # Properties
+    @property
+    def players(self) -> set[str]:
+        return self.__players
+    
+    @property
+    def questions(self) -> list[Question]:
+        return self.__answered_questions + self.__questions
+
+    @property
+    def rounds(self) -> int:
+        return self.__rounds
+
+    @property
+    def started(self) -> bool:
         return self.__started
+    
+    @property
+    def ended(self) -> bool:
+        return self.started and (len(self.__questions) == 0)
---
+++ test_quiz.py
@@ -0,0 +1,129 @@
+from quiz import Quiz
+
+def test_init_match():
+    quiz = Quiz("Esteban")
+    assert not quiz.init_match(2)
+    assert not quiz.started
+
+    quiz.register_player("Johan")
+    assert not quiz.init_match(0)
+    assert not quiz.started
+
+    assert quiz.init_match(2)
+    assert quiz.started
+    assert len(quiz.questions) == 4
+
+    assert not quiz.init_match(2)
+    assert quiz.started
+
+def test_register_player():
+    quiz = Quiz("Esteban")
+    assert len(quiz.players) == 1
+
+    assert quiz.register_player("Johan")
+    assert len(quiz.players) == 2
+
+    assert not quiz.register_player("Johan")
+    assert len(quiz.players) == 2
+
+    quiz.init_match(2)
+
+    assert not quiz.register_player("Mateo")
+    assert len(quiz.players) == 2
+
+def test_answer_question():
+    quiz = Quiz("Esteban")
+    quiz.register_player("Johan")
+    quiz.register_player("Mateo")
+
+    quiz.init_match(1)
+
+    actual_question = quiz.actual_question()
+    assert quiz.answer_question("Esteban","A")
+    assert quiz.answer_question("Johan","B")
+    assert not quiz.answer_question("Samuel","C")
+    assert not quiz.answer_question("Johan","D")
+    assert actual_question == quiz.actual_question()
+    assert quiz.answer_question("Mateo","A")
+    assert actual_question != quiz.actual_question()
+
+    actual_question = quiz.actual_question()
+    assert quiz.answer_question("Esteban","D")
+    assert quiz.answer_question("Johan","C")
+    assert not quiz.answer_question("Samuel","C")
+    assert not quiz.answer_question("Johan","D")
+    assert actual_question == quiz.actual_question()
+    assert quiz.answer_question("Mateo","C")
+    assert actual_question != quiz.actual_question()
+
+    actual_question = quiz.actual_question()
+    assert quiz.answer_question("Esteban","B")
+    assert quiz.answer_question("Johan","A")
+    assert not quiz.answer_question("Samuel","C")
+    assert not quiz.answer_question("Johan","D")
+    assert actual_question == quiz.actual_question()
+    assert quiz.answer_question("Mateo","D")
+    assert actual_question != quiz.actual_question()
+
+    actual_question = quiz.actual_question()
+    assert not actual_question
+    assert not quiz.answer_question("Esteban","A")
+    assert quiz.ended
+
+    given_answers = quiz.questions[0].answers
+    assert len(given_answers) == 3
+    assert given_answers["Esteban"] == "A"
+    assert given_answers["Johan"] == "B"
+    assert given_answers["Mateo"] == "A"
+
+    given_answers = quiz.questions[1].answers
+    assert len(given_answers) == 3
+    assert given_answers["Esteban"] == "D"
+    assert given_answers["Johan"] == "C"
+    assert given_answers["Mateo"] == "C"
+
+    given_answers = quiz.questions[2].answers
+    assert len(given_answers) == 3
+    assert given_answers["Esteban"] == "B"
+    assert given_answers["Johan"] == "A"
+    assert given_answers["Mateo"] == "D"
+
+def test_scores():
+    def answer_factory(quiz: Quiz):
+        actual_player = quiz.actual_question().player
+        if actual_player == "Esteban":
+            quiz.answer_question("Esteban","A")
+            quiz.answer_question("Johan","A")
+            quiz.answer_question("Mateo","B")
+        elif actual_player == "Johan":
+            quiz.answer_question("Esteban","B")
+            quiz.answer_question("Johan","A")
+            quiz.answer_question("Mateo","C")
+        elif actual_player == "Mateo":
+            quiz.answer_question("Esteban","D")
+            quiz.answer_question("Johan","D")
+            quiz.answer_question("Mateo","D")
+
+    quiz = Quiz("Esteban")
+    quiz.register_player("Johan")
+    quiz.register_player("Mateo")
+
+    quiz.init_match(2)
+
+    answer_factory(quiz)
+    answer_factory(quiz)
+    answer_factory(quiz)
+
+    scores = quiz.scores()
+    assert scores["Esteban"] == 1
+    assert scores["Johan"] == 2
+    assert scores["Mateo"] == 0
+
+    answer_factory(quiz)
+    answer_factory(quiz)
+    answer_factory(quiz)
+
+    scores = quiz.scores()
+    assert scores["Esteban"] == 2
+    assert scores["Johan"] == 4
+    assert scores["Mateo"] == 0
